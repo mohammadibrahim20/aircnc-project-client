72555c1129adc74aae44d7fb1e58acf306c7db94
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-05-30 19:02:15 +0600

    Login Reg. done

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 import Main from "../layouts/Main";
 
 export const router = createBrowserRouter([
@@ -13,4 +15,6 @@ export const router = createBrowserRouter([
       },
     ],
   },
+  {path: "/login", element: <Login />},
+  {path: "/signup", element: <SignUp />},
 ]);
